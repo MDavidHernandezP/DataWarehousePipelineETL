0054c8e9ead01bd1f34c52f36e12733ba366f40d
MDavidHernandezP <mdavidhernandezp@gmail.com>
2025-04-07 15:46:07 -0600

    Sqlite manages db's a little bit different from the others.

---
+++ DDL/SQLite/databaseCreationQuery.sql
@@ -0,0 +1,2 @@
+-- For sqlite databases, we need to create the database file first that will be stored in the
+-- data directory. The database file will be created when the connection to the database is opened.
